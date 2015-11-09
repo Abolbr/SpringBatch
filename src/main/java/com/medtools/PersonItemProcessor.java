baffdc9ca6b1e160d356c580b00fd75ae49a203a
Ibraheem <Abolbr1989@gmail.com>
2015-11-09 10:40:19 +0200

    first commit

---
+++ src/main/java/com/medtools/PersonItemProcessor.java
@@ -0,0 +1,11 @@
+package com.medtools;
+
+
+import org.springframework.batch.item.ItemProcessor;
+
+public class PersonItemProcessor implements ItemProcessor<Person,Person> {
+    @Override
+    public Person process(Person person) throws Exception {
+        return null;
+    }
+}
